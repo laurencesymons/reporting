7ad45c8f2cc87ae51cc42c0a3afb40f32598ee78
Harrajan <44954578+Harrajan@users.noreply.github.com>
2020-07-15 22:03:38 +0100

    adding json file

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -90,8 +90,8 @@ class ImportsController < ApplicationController
     quarter = [year, quarters[month]].join("")
 
     raw_r01 = RawRecruitment.select(:day, :dispcode).where(["import_id = ?", import_id]).group(:day).count(:dispcode)
-    raw_r02 = RawRecruitment.select(:day, :pstatu_date).where(["import_id = ? AND pstatu_date IS NOT null", import_id]).group(:day).count(:pstatu_date)
-    raw_recruited = RawRecruitment.select(:day, :datetime).where(["import_id = ? AND datetime IS NOT null", import_id]).group(:day).count(:datetime)
+    raw_r02 = RawRecruitment.select(:day, :pstatu_date).where(["import_id = ?", import_id]).group(:day).count(:pstatu_date)
+    raw_recruited = RawRecruitment.select(:day, :datetime).where(["import_id = ?", import_id]).group(:day).count(:datetime)
 
     format_raw(year,month,raw_r01, raw_r02, raw_recruited).each do |timestamp, data|
       r01_Completers, r02_Starters, recruited = data
