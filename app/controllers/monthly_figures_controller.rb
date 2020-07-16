f7e85388ad9ee88beda9728382ede1d16d9509be
Harrajan <44954578+Harrajan@users.noreply.github.com>
2020-07-16 18:46:26 +0100

    adding raw data

--- app/controllers/monthly_figures_controller.rb
+++ app/controllers/monthly_figures_controller.rb
@@ -1,6 +1,6 @@
 class MonthlyFiguresController < ApplicationController
   def index
     @monthly_figures = DailyFigure.group_by_month(:timestamp, format: "%B %Y")
-    @quarter_figures = DailyFigure.group_by_quarter(:timestamp, format: "%B %Y")
+    @quarter_figures = DailyFigure.group_by_quarter(:timestamp, format: "%q %Y")
   end
 end
